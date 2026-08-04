56c90213449e33de8b774e95661a04706af4ed1d
aroojfatima-315 <aroojfatima7017@gmail.com>
2026-08-04 23:00:29 +0500

    update 2 files

---
+++ script.js
@@ -0,0 +1,21 @@
+function addToCart(name, price, image) {
+
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    let existingProduct = cart.find(item => item.name === name);
+
+    if (existingProduct) {
+        existingProduct.quantity++;
+    } else {
+        cart.push({
+            name: name,
+            price: price,
+            image: image,
+            quantity: 1
+        });
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    alert(name + " added to cart! 🛒");
+}
